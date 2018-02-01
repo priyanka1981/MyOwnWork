9ab48e0c53e6a55303bca3a87b0e0c9e8f5dcda3
Priyanka Sharma <priyankasharma181@gmail.com>
2018-02-01 06:44:12 -0500

    Commiting link list

---
+++ DataStructure/LinkList/LinkList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace DataStructure.LinkList
+{
+    public class LinkList
+    {
+        private Node head;
+        private Node tail;
+        private Node current;
+
+        public LinkList()
+        {
+        }
+
+
+
+        public void Add(Node node, int _data)
+        {
+            if (node.head != null)
+                head = node;
+            else
+                current.next = node;
+        }
+
+    }
+}
---
+++ DataStructure/LinkList/Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace DataStructure.LinkList
+{
+    public class Node
+    {
+        public Node(Node node)
+        {
+           
+        }
+
+    
+    private int data;
+    public Node current;
+    public Node head;
+    public Node tail;
+    public Node next;
+    public Node(int _data, Node node)
+    {
+            
+        this.data = _data;
+    }
+
+    public Node(int _data)
+    {
+             
+        this.data = _data;
+    }
+
+    public void Add(Node node, int _data)
+    {
+        if (head != null)
+            head = node;
+        else
+            current.next = node;
+    }
+}
+}
