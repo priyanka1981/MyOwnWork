ee86e1d248e66a21be75c66e12d63bd4c98c7425
Priyanka Sharma <priyankasharma181@gmail.com>
2018-03-16 16:06:56 -0400

    Commit

--- DataStructure/Parallel/DataSyncronization.cs
+++ DataStructure/Parallel/DataSyncronization.cs
@@ -1,30 +1,66 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace DataStructure.Parallel
 {
     public class DataSyncronization
     {
+        private static object FirstObject;
+        private static object SecondObject;
         public DataSyncronization()
         {
           
 
         }
 
-        static void Main(string[] args){
+        private static void ThreadJob(){
+            lock (FirstObject){
+                Console.WriteLine("Locking first Object from ThreaJob");
+                lock(SecondObject){
+                    Console.WriteLine("Locking second object from ThreadJob");
+                }
+            }
+        }
+
+
+
+        static void Main_DataSync(string[] args){
            //bool (100 + 1.0 / 3) - 100 == 1.0 / 3
-            var a = new Action(() => Console.Write("a"));
-            var b = new Action(() => Console.Write("a"));
-            var c = new Action(() => Console.Write("a"));
 
-            System.Threading.Tasks.Parallel.Invoke(a,b,c);
-            System.Threading.Tasks.Parallel.For(1, 11, i =>
-                                                  Console.WriteLine($"{i * i}"));
+            Thread t = new Thread(ThreadJob);
+            t.Name = "test thread";
+            t.Start();
+            Thread.Sleep(1500);
+            lock (SecondObject)
+            {
+                Console.WriteLine("Locking second Object from main....");
+                lock (FirstObject)
+                {
+                    Console.WriteLine("Locking first object from main...");
+                }
+            }
 
-            string[] words = { "what", "a", "night" };
-            System.Threading.Tasks.Parallel.ForEach(words, word =>
-                                                    Console.WriteLine($"{word} has length {word.Length}, task {Task.CurrentId}"));
+
+
+            Console.Read();
+
+
+
+
+
+            //var a = new Action(() => Console.Write("a"));
+            //var b = new Action(() => Console.Write("a"));
+            //var c = new Action(() => Console.Write("a"));
+
+            //System.Threading.Tasks.Parallel.Invoke(a,b,c);
+            //System.Threading.Tasks.Parallel.For(1, 11, i =>
+            //                                      Console.WriteLine($"{i * i}"));
+
+            //string[] words = { "what", "a", "night" };
+            //System.Threading.Tasks.Parallel.ForEach(words, word =>
+                                                    //Console.WriteLine($"{word} has length {word.Length}, task {Task.CurrentId}"));
 
             //Dictionary<string,string> dict = new Dictionary<string, string>();
             //var tasks = new List<Task>();
@@ -61,6 +97,12 @@ namespace DataStructure.Parallel
             public void Withdraw(int amount){
                 balance -= amount;
             }
+        }
+
+        public void Method1(){
+            lock (FirstObject)
+                Console.Write("Locking m1 from first");
+            
         }
 
 
---
+++ DataStructure/Serialization/SerializationQuickStart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+namespace DataStructure.Serialization
+{
+    public class SerializationQuickStart
+    {
+        public SerializationQuickStart()
+        {
+        }
+
+
+        static void Main_DataSync(string[] args)
+        {
+            
+        }
+
+        private static MemoryStream SerializeToMemory(object objGraph){
+            MemoryStream stream = new MemoryStream();
+            //System.Runtime.Serialization..BinaryFormatter formatter = new binaryFormatter();
+            //formatter.
+            return stream;
+            
+        }
+    }
+}
