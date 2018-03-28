180b5fcfa072601c65684425ac5acd7a9eae271b
Priyanka Sharma <priyankasharma181@gmail.com>
2018-03-28 08:42:21 -0400

    adding more commits

---
+++ DataStructure/AmazonInterviewQuestions/PractiseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DataStructure.AmazonInterviewQuestions
+{
+    public class PractiseClass
+    {
+        public PractiseClass()
+        {
+        }
+
+        public void occureancebyPercentage(int[] array)
+        {
+            
+        }
+    }
+}
